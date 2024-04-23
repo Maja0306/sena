9ca0c5f8798a4631708242358113e88cb370a23d
Maja0306 <Majand01@iba.dk>
2024-04-23 16:08:36 +0200

    dd

--- script.js
+++ script.js
@@ -1,64 +1,3 @@
-// ! Labyrint 
-
-const svg = document.querySelector('svg.vector')
-const rect = svg.querySelector('rect') 
-
-const scroll = () => {
-  const distance = window.scrollY / 2 
-  const totalDistance = svg.clientHeight * 1.45
-
-  const percentage = distance / totalDistance
-
-  const rectLenght = rect.getTotalLength() * 2
-
-  rect.style.strokeDasharray = `${rectLenght}`
-  rect.style.strokeDashoffset = `${rectLenght * (1 - percentage)} `
-} 
-
-scroll()
-window.addEventListener("scroll", scroll)
-
-
-// ! Hængelås som bevæger sig med musen (inspireret af SuperHi på Youtube)
-
-const section = document.querySelector('div.mouse-effect')
-
-const image = section.querySelector('div.img')
-const shadow = section.querySelector('div.shadow')
-
-let aimX = 0
-let aimY = 0
-let imageX = 0
-let imageY = 0
-let shadowX = 0
-let shadowY = 0
-let backgroundX = 0
-let backgroundY = 0
-
-const move = (event) => {
-  aimX = 2 * event.clientX / window.innerWidth - 1
-  aimY = 2 * event.clientY / window.innerHeight - 1
-} // gør så billede følger musen fra midten af skærmen. 
-
-const animate = () => {
-  imageX += (aimX - imageX) * 0.1
-  imageY += (aimY - imageY) * 0.1
-
-  shadowX += (aimX - shadowX) * 0.05
-  shadowY += (aimY - shadowY) * 0.05
-
-  backgroundX += (aimX - backgroundX) * 0.4
-  backgroundY += (aimY - backgroundY) * 0.4
-
-  image.style.transform = `translate(${4 * imageX}rem, ${4 * imageY}rem)` 
-  shadow.style.transform = `translate(${4 * shadowX}rem, ${4 * shadowY}rem)`
-
-  requestAnimationFrame(animate)
-}
-
-animate()
-document.addEventListener('mousemove', move)
-
 // ! Få spørgsmål og svar til at flyve ind på siden
 
 // ! Scroll ned på siden ved hjælp af pilen
@@ -176,3 +115,31 @@ document.addEventListener('click', e => {
 })
 
 // Hvis man klikker på en boks, så skal senarie 1 rykke ud og senarie 3 skal rykke ind
+
+
+//! Knapper i headeren
+
+const buttonQuestion = document.querySelector('.button-header-question');
+const pTagInsideButtonQuestion = buttonQuestion.querySelector('p');
+const imgTagInsideButtonQuestion = buttonQuestion.querySelector('img');  
+const boxQuestion = document.querySelector('.question-box');
+
+buttonQuestion.addEventListener('click', rotateAndColorChange = () => {
+  pTagInsideButtonQuestion.classList.toggle('active');
+  imgTagInsideButtonQuestion.classList.toggle('active-2'); 
+  boxQuestion.classList.toggle('active-3');
+  setTimeout(() => {
+    if (pTagInsideButtonQuestion.classList.contains('active')) {
+      boxQuestion.style.height = 'auto';
+      const fullHeight = boxQuestion.scrollHeight + 'px';
+      boxQuestion.style.height = '0';
+      requestAnimationFrame(() => {
+        boxQuestion.style.transition = 'height 0.2s ease-in-out';
+        boxQuestion.style.height = fullHeight;
+      });
+    } else {
+      boxQuestion.style.transition = 'height 0.1s ease-in-out';
+      boxQuestion.style.height = '0';
+    }
+  }, 500);
+});
---
+++ test.js
@@ -0,0 +1,6 @@
+const hej = document.querySelector('p');
+const box = document.querySelector('div');
+
+box.addEventListener('click', rotate = () => {
+  hej.classList.add('active');
+});
